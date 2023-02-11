450cd3259eff6914fbcb8bafa51a1af3edc939e1
Prosper Ikechukwu <mrhendrix62@gmail.com>
2023-02-11 22:15:21 +0100

    Feat: Completed the Save Task functionality and switched the task data to a state

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,18 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Form.css'
 
-const Form = () => {
+const Form = ({saveTask}) => {
+	const [taskName, setTaskName] = useState('')
+	const [taskDetails, setTaskDetails] = useState('')
+
+	const submitHandler = (e) => {
+		e.preventDefault()
+
+		if (!taskName) {
+			alert('Enter Task Name')
+		}
+		if (!taskDetails) {
+			alert('Enter Details of Task')
+		}
+
+		saveTask({ taskName, taskDetails })
+		
+		setTaskName('')
+		setTaskDetails('')
+	}
+
   return (
-		<form action="" className='form'>
+		<form action="" className="form" onSubmit={submitHandler}>
 			<div className="form-controls">
 				<label htmlFor="taskName"> Task Name: </label>
-				<input type="text" name="taskName" id="taskName" placeholder='Enter Task Name'/>
+				<input
+					type="text"
+					name="taskName"
+					id="taskName"
+					value={taskName}
+					onChange={(e) => setTaskName(e.target.value)}
+					placeholder="Enter Task Name"
+				/>
 			</div>
 
 			<div className="form-controls">
 				<label htmlFor="taskDetails"> Task Details: </label>
-				<input type="text" name="taskDetails" id="taskDetails" placeholder='Enter Task Details'/>
+				<input
+					type="text"
+					name="taskDetails"
+					id="taskDetails"
+					value={taskDetails}
+					onChange={(e) => setTaskDetails(e.target.value)}
+					placeholder="Enter Task Details"
+				/>
 			</div>
+
+			<input type="submit" value="Save Task" className="btn" />
 		</form>
   );
 }
--- src/components/TasksContainer.jsx
+++ src/components/TasksContainer.jsx
@@ -1,25 +1,7 @@
 import React from 'react';
 import Task from './Task/Task';
 
-const TasksContainer = () => {
-	const tasks = [
-		{
-			id: 1,
-			taskName: 'Dentist Appointment',
-			taskDetails: 'Friday by 4PM',
-		},
-		{
-			id: 2,
-			taskName: 'Date Night With My Ex',
-			taskDetails: 'Sunday by 8AM',
-		},
-		{
-			id: 3,
-			taskName: 'Talk with Tappi',
-			taskDetails: 'Monday by 10AM',
-		}
-	];
-
+const TasksContainer = ({ tasks }) => {
 
 	return (
 		<div
